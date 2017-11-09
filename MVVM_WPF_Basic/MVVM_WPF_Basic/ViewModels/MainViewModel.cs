06697803aa697456db751fc090cd0c23f1df26f7
Zakaria Mansouri <ZAQO.cs@outlook.com>
2017-11-09 13:58:47 +0100

    Added ViewModels/MainViewModel.cs

---
+++ MVVM_WPF_Basic/MVVM_WPF_Basic/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MVVM_WPF_Basic.ViewModels.Basis;
+
+namespace MVVM_WPF_Basic.ViewModels
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
