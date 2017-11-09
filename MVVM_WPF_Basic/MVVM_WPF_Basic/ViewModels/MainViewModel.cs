b527bd0cf55158ccd38412ad6a50866ced1aa617
Zakaria Mansouri <ZAQO.cs@outlook.com>
2017-11-09 18:08:13 +0100

    Added Accounts ObservableCollection to MainViewModel

--- MVVM_WPF_Basic/MVVM_WPF_Basic/ViewModels/MainViewModel.cs
+++ MVVM_WPF_Basic/MVVM_WPF_Basic/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using MVVM_WPF_Basic.ViewModels.Basis;
+﻿using MVVM_WPF_Basic.Models;
+using MVVM_WPF_Basic.ViewModels.Basis;
+using System.Collections.ObjectModel;
 
 namespace MVVM_WPF_Basic.ViewModels
 {
@@ -6,6 +8,8 @@ namespace MVVM_WPF_Basic.ViewModels
     {
         public MainViewModel()
         {
+            Accounts = new ObservableCollection<Account> { };
         }
+        public ObservableCollection<Account> Accounts { get; set; }
     }
 }
