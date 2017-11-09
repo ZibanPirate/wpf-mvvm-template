877d0c6431caf1d69f2425cf391f56b3f3f54d4b
Zakaria Mansouri <ZAQO.cs@outlook.com>
2017-11-09 18:45:12 +0100

    Added the ICommand AddItem and its methode

--- MVVM_WPF_Basic/MVVM_WPF_Basic/ViewModels/MainViewModel.cs
+++ MVVM_WPF_Basic/MVVM_WPF_Basic/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using MVVM_WPF_Basic.Models;
 using MVVM_WPF_Basic.ViewModels.Basis;
+using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace MVVM_WPF_Basic.ViewModels
 {
@@ -11,5 +13,19 @@ namespace MVVM_WPF_Basic.ViewModels
             Accounts = new ObservableCollection<Account> { };
         }
         public ObservableCollection<Account> Accounts { get; set; }
+
+        public ICommand AddItem => new DelegateCommand(_AddItem);
+        void _AddItem()
+        {
+            Random rnd = new Random();
+            string[] FirstNames = { "Zakaria", "Ghani", "Saleh", "Wassim", "Khalil" }
+                , LastNames = { "Mansouri", "Nacer", "Hadef", "Djeffel", "Soltan" };
+            Accounts.Add(new Account
+            {
+                Id = (uint)rnd.Next(),
+                FirstName = FirstNames[rnd.Next(FirstNames.Length)],
+                LastName = LastNames[rnd.Next(LastNames.Length)]
+            });
+        }
     }
 }
