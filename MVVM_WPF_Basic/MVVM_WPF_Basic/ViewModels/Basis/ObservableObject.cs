c3259dfc82ca5c73570dd039149517925b70fb03
Zakaria Mansouri <ZAQO.cs@outlook.com>
2017-11-09 13:57:59 +0100

    Added ViewModels/Basis/ObservableObject.cs

---
+++ MVVM_WPF_Basic/MVVM_WPF_Basic/ViewModels/Basis/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace MVVM_WPF_Basic.ViewModels.Basis
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
